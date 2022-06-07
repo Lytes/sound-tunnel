2454f92a2e0c2b3f3001f509cbd24f389b772dac
Olaitan ayoola <Laitanayoola@gmail.com>
2022-06-07 09:31:48 +0100

    Update works.py

--- works.py
+++ works.py
@@ -7,6 +7,7 @@ import spotipy
 import config as cfg
 spotify = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=cfg.CLIENT_ID, client_secret=cfg.CLIENT_SECRET, redirect_uri=cfg.REDIRECT_URI, scope=cfg.SCOPE))
 # added print(auth_url) to line 435 of /usr/local/lib/python3.10/dist-packages/spotipy/oauth2.py so that the script would print the link
+# Why? Because wsl2 can't open  sucks 
 user_deet = spotify.me() #gets user details
 user_playlists = spotify.current_user_playlists() #gets user playlists
 
